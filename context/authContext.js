419f75804bdd0b99af3a2b418ce7d463f14f0d33
ahmedaghadi <ahmedaghadi123@gmail.com>
2023-03-21 22:34:15 +0530

    fix: push user to login if not authorized

--- context/authContext.js
+++ context/authContext.js
@@ -2,13 +2,13 @@ import React, { createContext } from "react";
 import { useContext, useState, useEffect, useRef } from "react";
 import { useRouter } from "next/router";
 import {
-  signInWithEmailAndPassword,
-  signInWithPopup,
-  createUserWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged,
-  GoogleAuthProvider,
-  sendEmailVerification,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    createUserWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
+    GoogleAuthProvider,
+    sendEmailVerification,
 } from "firebase/auth";
 import { auth } from "../firebase/firebaseConfig";
 import { publicPaths } from "../constants/publicPaths";
@@ -18,124 +18,126 @@ const googleProvider = new GoogleAuthProvider();
 const AuthContext = createContext();
 
 export const useAuth = () => {
-  return useContext(AuthContext);
+    return useContext(AuthContext);
 };
 
 export function AuthProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState(null);
-  const userInfo = useRef();
-  const router = useRouter();
-  const user = auth.currentUser;
-
-  const [authorized, setAuthorized] = useState(false);
-
-  const SignUp = (email, password, confirmPassword) => {
-    if (password === confirmPassword) {
-      createUserWithEmailAndPassword(auth, email, password)
-        .then(async () => {
-          router.push("/dashboard");
-          verifyEmail();
-        })
-        .catch((err) => alert(err.message));
-    } else {
-      alert("Passwords don't match");
-    }
-  };
-
-  const LogIn = (loginEmail, loginPassword) => {
-    signInWithEmailAndPassword(auth, loginEmail, loginPassword)
-      .then(async () => {
-        router.push("/dashboard");
-        verifyEmail();
-      })
-      .catch((err) => alert(err.message));
-  };
-
-  const LoginWithGoogle = () => {
-    signInWithPopup(auth, googleProvider)
-      .then(async () => {
-        router.push("/dashboard");
-        verifyEmail();
-      })
-      .catch((err) => alert(err.message));
-  };
-
-  const SignUpWithGoogle = () => {
-    signInWithPopup(auth, googleProvider)
-      .then(async () => {
-        router.push("/dashboard");
-        verifyEmail();
-      })
-      .catch((err) => alert(err.message));
-  };
-
-  const verifyEmail = async () => {
+    const [currentUser, setCurrentUser] = useState(null);
+    const userInfo = useRef();
+    const router = useRouter();
     const user = auth.currentUser;
-    if (!user.emailVerified)
-      await sendEmailVerification(auth.currentUser).then(() => {
-        const user = auth.currentUser;
-        if (user) {
-          console.log(user.emailVerified);
+
+    const [authorized, setAuthorized] = useState(false);
+
+    const SignUp = (email, password, confirmPassword) => {
+        if (password === confirmPassword) {
+            createUserWithEmailAndPassword(auth, email, password)
+                .then(async () => {
+                    router.push("/dashboard");
+                    verifyEmail();
+                })
+                .catch((err) => alert(err.message));
         } else {
-          console.log("no user found");
+            alert("Passwords don't match");
         }
-      });
-  };
+    };
 
-  const SignOut = () => {
-    signOut(auth)
-      .then(() => router.push("/"))
-      .catch((err) => alert(err.message));
-  };
+    const LogIn = (loginEmail, loginPassword) => {
+        signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+            .then(async () => {
+                router.push("/dashboard");
+                verifyEmail();
+            })
+            .catch((err) => alert(err.message));
+    };
 
-  useEffect(() => {
-    const unscubscribe = onAuthStateChanged(auth, async (user) => {
-      setCurrentUser(user);
-    });
-    return unscubscribe;
-  }, []);
+    const LoginWithGoogle = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(async () => {
+                router.push("/dashboard");
+                verifyEmail();
+            })
+            .catch((err) => alert(err.message));
+    };
 
-  useEffect(() => {
-    const user = auth.currentUser;
-    setCurrentUser(user);
-  }, []);
+    const SignUpWithGoogle = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(async () => {
+                router.push("/dashboard");
+                verifyEmail();
+            })
+            .catch((err) => alert(err.message));
+    };
 
-  useEffect(() => {
-    const authCheck = () => {
-      if (!user && !publicPaths.includes(router.asPath.split("?")[0])) {
-        setAuthorized(false);
-        // dispatch(setRedirectLink({ goto: router.asPath }));
-        void router.push({
-          pathname: "/login",
+    const verifyEmail = async () => {
+        const user = auth.currentUser;
+        if (!user.emailVerified)
+            await sendEmailVerification(auth.currentUser).then(() => {
+                const user = auth.currentUser;
+                if (user) {
+                    console.log(user.emailVerified);
+                } else {
+                    console.log("no user found");
+                }
+            });
+    };
+
+    const SignOut = () => {
+        signOut(auth)
+            .then(() => router.push("/"))
+            .catch((err) => alert(err.message));
+    };
+
+    useEffect(() => {
+        const unscubscribe = onAuthStateChanged(auth, async (user) => {
+            setCurrentUser(user);
         });
-      } else {
-        setAuthorized(true);
-      }
+        return unscubscribe;
+    }, []);
+
+    useEffect(() => {
+        const user = auth.currentUser;
+        setCurrentUser(user);
+    }, []);
+
+    useEffect(() => {
+        const authCheck = () => {
+            if (!user && !publicPaths.includes(router.asPath.split("?")[0])) {
+                setAuthorized(false);
+                // dispatch(setRedirectLink({ goto: router.asPath }));
+                router.push({
+                    pathname: "/login",
+                });
+            } else {
+                setAuthorized(true);
+            }
+        };
+
+        authCheck();
+
+        const preventAccess = () => setAuthorized(false);
+
+        router.events.on("routeChangeStart", preventAccess);
+        router.events.on("routeChangeComplete", authCheck);
+
+        return () => {
+            router.events.off("routeChangeStart", preventAccess);
+            router.events.off("routeChangeComplete", authCheck);
+        };
+    }, [router, router.events, currentUser]);
+
+    const value = {
+        currentUser,
+        LogIn,
+        LoginWithGoogle,
+        SignOut,
+        SignUp,
+        userInfo,
+        SignUpWithGoogle,
+        sendEmailVerification,
     };
 
-    authCheck();
-
-    const preventAccess = () => setAuthorized(false);
-
-    router.events.on("routeChangeStart", preventAccess);
-    router.events.on("routeChangeComplete", authCheck);
-
-    return () => {
-      router.events.off("routeChangeStart", preventAccess);
-      router.events.off("routeChangeComplete", authCheck);
-    };
-  }, [router, router.events, currentUser]);
-
-  const value = {
-    currentUser,
-    LogIn,
-    LoginWithGoogle,
-    SignOut,
-    SignUp,
-    userInfo,
-    SignUpWithGoogle,
-    sendEmailVerification,
-  };
-
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+    return (
+        <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+    );
 }
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,13 +1,13 @@
 import Head from "next/head";
 import React from "react";
 import {
-  TextInput,
-  Checkbox,
-  Button,
-  Group,
-  Box,
-  PasswordInput,
-  Anchor,
+    TextInput,
+    Checkbox,
+    Button,
+    Group,
+    Box,
+    PasswordInput,
+    Anchor,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { Layout } from "../../components/Layout";
@@ -16,108 +16,114 @@ import { auth } from "../../firebase/firebaseConfig";
 import { useAuth } from "../../context/authContext";
 
 export default function Home() {
-  const { SignUp, SignUpWithGoogle } = useAuth();
+    const { SignUp, SignUpWithGoogle } = useAuth();
 
-  const SignUpUser = async (formValues) => {
-    try {
-      if (!formValues) return;
-      console.log(formValues);
-      await SignUp(formValues.email, formValues.password);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    const SignUpUser = async (formValues) => {
+        try {
+            if (!formValues) return;
+            console.log(formValues);
+            await SignUp(formValues.email, formValues.password);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-  const SignUpUserGoogle = async () => {
-    try {
-      console.log("Signing Up");
-      await SignUpWithGoogle();
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    const SignUpUserGoogle = async () => {
+        try {
+            console.log("Signing Up");
+            await SignUpWithGoogle();
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-  const form = useForm({
-    validateInputOnChange: true,
-    initialValues: {
-      email: "",
-      password: "",
-      confirmPassword: "",
-    },
+    const form = useForm({
+        validateInputOnChange: true,
+        initialValues: {
+            email: "",
+            password: "",
+            confirmPassword: "",
+        },
 
-    validate: {
-      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
-      password: (value) =>
-        value.length < 6 ? "Password must be at least 6 characters" : null,
-      confirmPassword: (value, values) =>
-        value !== values.password ? "Passwords did not match" : null,
-    },
-  });
+        validate: {
+            email: (value) =>
+                /^\S+@\S+$/.test(value) ? null : "Invalid email",
+            password: (value) =>
+                value.length < 6
+                    ? "Password must be at least 6 characters"
+                    : null,
+            confirmPassword: (value, values) =>
+                value !== values.password ? "Passwords did not match" : null,
+        },
+    });
 
-  return (
-    <Layout>
-      <Head>
-        <title>Owlearn</title>
-        <meta name="description" content="Owlearn" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <Box
-          p="xl"
-          maw={500}
-          mx="auto"
-          sx={
-            {
-              // display: "flex",
-              // flexDirection: "column",
-              // // alignItems: "center",
-              // justifyContent: "center",
-              // height: "100vh",
-            }
-          }
-        >
-          <form
-            onSubmit={form.onSubmit((values) => {
-              SignUpUser(values);
-              console.log(values);
-            })}
-          >
-            <TextInput
-              withAsterisk
-              label="Email"
-              placeholder="your@email.com"
-              {...form.getInputProps("email")}
-            />
+    return (
+        <Layout>
+            <Head>
+                <title>Owlearn</title>
+                <meta name="description" content="Owlearn" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+                <Box
+                    p="xl"
+                    maw={500}
+                    mx="auto"
+                    sx={
+                        {
+                            // display: "flex",
+                            // flexDirection: "column",
+                            // // alignItems: "center",
+                            // justifyContent: "center",
+                            // height: "100vh",
+                        }
+                    }
+                >
+                    <form
+                        onSubmit={form.onSubmit((values) => {
+                            SignUpUser(values);
+                            console.log(values);
+                        })}
+                    >
+                        <TextInput
+                            withAsterisk
+                            label="Email"
+                            placeholder="your@email.com"
+                            {...form.getInputProps("email")}
+                        />
 
-            <PasswordInput
-              label="Password"
-              placeholder="Password"
-              {...form.getInputProps("password")}
-            />
+                        <PasswordInput
+                            label="Password"
+                            placeholder="Password"
+                            {...form.getInputProps("password")}
+                        />
 
-            <PasswordInput
-              mt="sm"
-              label="Confirm password"
-              placeholder="Confirm password"
-              {...form.getInputProps("confirmPassword")}
-            />
+                        <PasswordInput
+                            mt="sm"
+                            label="Confirm password"
+                            placeholder="Confirm password"
+                            {...form.getInputProps("confirmPassword")}
+                        />
 
-            <Group position="right" mt="md">
-              <Button type="submit">Signin</Button>
-              <Anchor href="/login">Login instead?</Anchor>
-            </Group>
-          </form>
-          <a>Or</a>
-          <Button
-            onClick={() => {
-              SignUpUserGoogle();
-            }}
-          >
-            Signup with Google
-          </Button>
-        </Box>
-      </main>
-    </Layout>
-  );
+                        <Group position="right" mt="md">
+                            <Button type="submit">Signin</Button>
+                            <Anchor href="/login">Login instead?</Anchor>
+                        </Group>
+                    </form>
+                    <a>Or </a>
+                    <Button
+                        onClick={() => {
+                            SignUpUserGoogle();
+                        }}
+                    >
+                        Signup with Google
+                    </Button>
+                </Box>
+            </main>
+        </Layout>
+    );
 }
