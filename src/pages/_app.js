6bddd514d1a9575d001dd0f1c26c080d65c5dd17
Angel Lakra <angel.lakra@chainlabs.in>
2023-06-16 15:05:09 +0530

    test commit with pre commit hook
    Signed-off-by: Angel Lakra <angel.lakra@chainlabs.in>

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,14 @@
-import "@/styles/globals.css";
-import "@rainbow-me/rainbowkit/styles.css";
+import '@/styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css'
 import {
     ConnectButton,
     getDefaultWallets,
     RainbowKitProvider,
-} from "@rainbow-me/rainbowkit";
-import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { polygon } from "wagmi/chains";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
+} from '@rainbow-me/rainbowkit'
+import { configureChains, createClient, WagmiConfig } from 'wagmi'
+import { polygon } from 'wagmi/chains'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
 
 const { chains, provider } = configureChains(
     [polygon],
@@ -16,18 +16,18 @@ const { chains, provider } = configureChains(
         alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID }),
         publicProvider(),
     ]
-);
+)
 
 const { connectors } = getDefaultWallets({
-    appName: "Owlearn",
+    appName: 'Owlearn',
     chains,
-});
+})
 
 const wagmiClient = createClient({
     autoConnect: true,
     connectors,
     provider,
-});
+})
 
 export default function App({ Component, pageProps }) {
     return (
@@ -36,5 +36,5 @@ export default function App({ Component, pageProps }) {
                 <Component {...pageProps} />
             </RainbowKitProvider>
         </WagmiConfig>
-    );
+    )
 }
