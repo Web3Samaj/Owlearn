04d8b13933fd91a43af46b8b80fd0ec095b42523
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-09-13 22:36:08 +0530

    feat: Owlearn ID service added

---
+++ src/services/contracts/owlearnId.ts
@@ -0,0 +1,84 @@
+import { useAccount, usePublicClient, useWalletClient } from 'wagmi'
+import { getAccount, getPublicClient, getWalletClient } from 'wagmi/actions'
+import { OWLEARN_ID_ABI, OWLEARN_ID_ADDRESS } from '../../constants/owlearnId'
+import { decodeFunctionResult, getContract } from 'viem'
+
+export const owlearnId = async () => {
+  const { address: account } = getAccount()
+  const publicClient = getPublicClient()
+  const walletClient = await getWalletClient()
+  if (!walletClient) {
+    return
+  }
+
+  const owlearnId = getContract({
+    address: OWLEARN_ID_ADDRESS,
+    abi: OWLEARN_ID_ABI,
+  })
+
+  const registerOwlearnId = async (userName: string) => {
+    try {
+      const data = await publicClient.simulateContract({
+        account,
+        address: OWLEARN_ID_ADDRESS,
+        abi: OWLEARN_ID_ABI,
+        functionName: 'register',
+        args: [userName],
+      })
+
+      if (!walletClient) {
+        return
+      }
+      const tx = await walletClient.writeContract(data.request)
+      console.log('Transaction Sent')
+      const transaction = await publicClient.waitForTransactionReceipt({
+        hash: tx,
+      })
+      console.log('Transaction completed')
+      console.log(transaction)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getUserName = async (
+    userAddress: `0x${string}`
+  ): Promise<string | undefined> => {
+    try {
+      const result = await publicClient.readContract({
+        account,
+        address: OWLEARN_ID_ADDRESS,
+        abi: OWLEARN_ID_ABI,
+        functionName: 'getNameRecordFromAddress',
+        args: [userAddress],
+      })
+      return result
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const geNameRecord = async (
+    userName: string
+  ): Promise<
+    | {
+        domain: string
+        user: `0x${string}`
+        tokenId: bigint
+      }
+    | undefined
+  > => {
+    try {
+      const result = await publicClient.readContract({
+        account,
+        address: OWLEARN_ID_ADDRESS,
+        abi: OWLEARN_ID_ABI,
+        functionName: 'getNameRecord',
+        args: [userName],
+      })
+      return result
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
