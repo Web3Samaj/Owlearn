dc450168fa5706de58a3e10186ee8efbeb22bdf4
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-09-14 14:21:48 +0530

    feat: graph Service functions completed

--- src/services/graph/graph.ts
+++ src/services/graph/graph.ts
@@ -1,5 +1,13 @@
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
-import { allCourseQuery } from './query'
+import {
+  allCourseQuery,
+  allEducatorQuery,
+  allEnrolledCourseQuery,
+  indivCertificateQuery,
+  indivCourseQuery,
+  indivEducatorQuery,
+  indivResourceQuery,
+} from './query'
 const APIURL =
   'https://api.studio.thegraph.com/query/46388/owlearn/version/latest'
 
@@ -10,11 +18,121 @@ const client = new ApolloClient({
 
 // Caching can be done too , for almost instant response
 
-export const getCourses = () => {
+export const getAllCourses = (courseToFetch: number) => {
   client
     .query({
       query: allCourseQuery,
-      variables: {},
+      variables: {
+        first: courseToFetch,
+      },
+    })
+    .then((res) => {
+      console.log(res.data)
+      return res.data
+    })
+    .catch((err) => {
+      console.error(err)
+      return err
+    })
+}
+
+export const getCourse = (courseId: string) => {
+  client
+    .query({
+      query: indivCourseQuery,
+      variables: {
+        id: courseId,
+      },
+    })
+    .then((res) => {
+      console.log(res.data)
+      return res.data
+    })
+    .catch((err) => {
+      console.error(err)
+      return err
+    })
+}
+
+export const getResource = (resorceId: string) => {
+  client
+    .query({
+      query: indivResourceQuery,
+      variables: {
+        id: resorceId,
+      },
+    })
+    .then((res) => {
+      console.log(res.data)
+      return res.data
+    })
+    .catch((err) => {
+      console.error(err)
+      return err
+    })
+}
+
+export const getCertificate = (certificateId: string) => {
+  client
+    .query({
+      query: indivCertificateQuery,
+      variables: {
+        id: certificateId,
+      },
+    })
+    .then((res) => {
+      console.log(res.data)
+      return res.data
+    })
+    .catch((err) => {
+      console.error(err)
+      return err
+    })
+}
+
+export const getEducator = (educatorId: string) => {
+  client
+    .query({
+      query: indivEducatorQuery,
+      variables: {
+        id: educatorId,
+      },
+    })
+    .then((res) => {
+      console.log(res.data)
+      return res.data
+    })
+    .catch((err) => {
+      console.error(err)
+      return err
+    })
+}
+
+export const getEnrolledCourses = (studentId: string) => {
+  client
+    .query({
+      query: allEnrolledCourseQuery,
+      variables: {
+        id: studentId,
+      },
+    })
+    .then((res) => {
+      console.log(res.data)
+      return res.data
+    })
+    .catch((err) => {
+      console.error(err)
+      return err
+    })
+}
+
+export const getAllEducators = (eduToFetch: string) => {
+  client
+    .query({
+      query: allEducatorQuery,
+      variables: {
+        id: eduToFetch,
+      },
     })
     .then((res) => {
       console.log(res.data)
