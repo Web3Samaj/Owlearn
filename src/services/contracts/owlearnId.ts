6530cae26e6730f824676dfeae8102e2d9f7cdd3
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-11-24 15:32:12 +0530

    feat: Added Contract check functions on the minter page

--- src/services/contracts/owlearnId.ts
+++ src/services/contracts/owlearnId.ts
@@ -7,6 +7,190 @@ import {
   prepareBlackListMerkleProof,
 } from '../utils/merkleProof'
 
+const getPrice = async (
+  userName: string,
+  toAddress: `0x${string}`
+): Promise<
+  | {
+      price: string
+      isFeeEnabled: boolean
+    }
+  | undefined
+> => {
+  const { address: account } = getAccount()
+  const publicClient = getPublicClient()
+  try {
+    const isFeeEnabled = await publicClient.readContract({
+      account,
+      address: OWLEARN_ID_ADDRESS,
+      abi: OWLEARN_ID_ABI,
+      functionName: 'isFeeEnabled',
+    })
+    if (isFeeEnabled) {
+      const result = await publicClient.readContract({
+        account,
+        address: OWLEARN_ID_ADDRESS,
+        abi: OWLEARN_ID_ABI,
+        functionName: 'getPrice',
+        args: [userName, toAddress],
+      })
+      const price = formatEther(result)
+      return { price, isFeeEnabled }
+    } else {
+      const price = '0'
+      return { price, isFeeEnabled }
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const checkAddressEligibility = async (
+  address: `0x${string}`
+): Promise<
+  | {
+      isEligible: boolean
+      isAllowListEnabled: boolean
+      allowListProof?: string[]
+    }
+  | undefined
+> => {
+  const { address: account } = getAccount()
+  const publicClient = getPublicClient()
+  try {
+    const isAllowListEnabled = await publicClient.readContract({
+      account,
+      address: OWLEARN_ID_ADDRESS,
+      abi: OWLEARN_ID_ABI,
+      functionName: 'isAllowListEnabled',
+    })
+    if (isAllowListEnabled) {
+      const allowListProof = await prepareAllowListMerkleProof(address)
+      const result = await publicClient.readContract({
+        account,
+        address: OWLEARN_ID_ADDRESS,
+        abi: OWLEARN_ID_ABI,
+        functionName: 'checkAllowlist',
+        args: [allowListProof, address],
+      })
+      return { isEligible: result, isAllowListEnabled, allowListProof }
+    } else {
+      return { isEligible: true, isAllowListEnabled }
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const checkUsernameEligibility = async (
+  username: string
+): Promise<
+  | {
+      isEligible: boolean
+      isBlackListEnabled: boolean
+      blackListProof?: string[]
+    }
+  | undefined
+> => {
+  const { address: account } = getAccount()
+  const publicClient = getPublicClient()
+  try {
+    const isBlackListEnabled = await publicClient.readContract({
+      account,
+      address: OWLEARN_ID_ADDRESS,
+      abi: OWLEARN_ID_ABI,
+      functionName: 'isBlackListEnabled',
+    })
+    if (isBlackListEnabled) {
+      const blackListProof = await prepareBlackListMerkleProof(username)
+
+      const blackListResult = await publicClient.readContract({
+        account,
+        address: OWLEARN_ID_ADDRESS,
+        abi: OWLEARN_ID_ABI,
+        functionName: 'checkBlackListName',
+        args: [blackListProof, username],
+      })
+
+      const availableHandleResult = await publicClient.readContract({
+        account,
+        address: OWLEARN_ID_ADDRESS,
+        abi: OWLEARN_ID_ABI,
+        functionName: 'checkHandle',
+        args: [username],
+      })
+
+      return {
+        isEligible: !blackListResult && availableHandleResult,
+        isBlackListEnabled,
+        blackListProof,
+      }
+    } else {
+      const availableHandleResult = await publicClient.readContract({
+        account,
+        address: OWLEARN_ID_ADDRESS,
+        abi: OWLEARN_ID_ABI,
+        functionName: 'checkHandle',
+        args: [username],
+      })
+      return { isEligible: availableHandleResult, isBlackListEnabled }
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const registerOwlearnId = async (
+  userName: string,
+  isAllowListEnabled: boolean,
+  isBlackListEnabled: boolean,
+  allowListProof?: string[],
+  blackListProof?: string[]
+): Promise<{ owlIdTokenId: bigint } | undefined> => {
+  try {
+    const { address: account } = getAccount()
+    const publicClient = getPublicClient()
+    const walletClient = await getWalletClient()
+    if (!walletClient) return
+    if (!account) return
+    console.log(walletClient)
+    console.log(publicClient)
+
+    const data = await publicClient.simulateContract({
+      account,
+      address: OWLEARN_ID_ADDRESS,
+      abi: OWLEARN_ID_ABI,
+      functionName: 'registerOwlId',
+      args: [
+        userName,
+        isAllowListEnabled ? allowListProof : [],
+        isBlackListEnabled ? blackListProof : [],
+      ],
+    })
+    console.log(data)
+    if (!walletClient) {
+      return
+    }
+    const tx = await walletClient.writeContract(data.request)
+    console.log('Transaction Sent')
+    const transaction = await publicClient.waitForTransactionReceipt({
+      hash: tx,
+    })
+    console.log('Transaction completed')
+    console.log(transaction)
+    return { owlIdTokenId: data?.result }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export {
+  checkAddressEligibility,
+  checkUsernameEligibility,
+  getPrice,
+  registerOwlearnId,
+}
+
 export const owlearnId = async () => {
   const { address: account } = getAccount()
   const publicClient = getPublicClient()
